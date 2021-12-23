1e2ca7ecaa2303ebda50c3f380ab6b9710b83c05
kgolikov <kgolikov@37bytes.com>
2021-12-23 11:20:49 +0500

    add CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/log', ((req, res) => {
     const from = req.query['from'] ?? req.headers['x-forwarded-for'] ?? req.socket.remoteAddress;
