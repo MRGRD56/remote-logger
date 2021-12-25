c572ccf9cb5b14a09ff42d4ebc616ad3f41c5554
MRGRD56 <Bukinist56@mail.ru>
2021-12-25 19:49:33 +0500

    fix client-app

--- test/client-app/index.ts
+++ test/client-app/index.ts
@@ -1,7 +1,7 @@
 import remoteLogger from "@mrgrd56/remote-logger";
 
-const logger = remoteLogger('http://localhost:33223', {
-    accessToken: 'ecd356f4-ab2f-4ef2-b09f-ce60ff5aa74d_'
+const logger = remoteLogger('http://localhost:3322', {
+    accessToken: 'ecd356f4-ab2f-4ef2-b09f-ce60ff5aa74d'
 });
 
 logger.log('hello world');
